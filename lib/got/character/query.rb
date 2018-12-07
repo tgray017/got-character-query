514a3b7520661289acc3f6242601fb0b874f6672
tgray017 <tgray017@gmail.com>
2018-12-07 23:20:04 +0000

    first commit

---
+++ lib/got/character/query.rb
@@ -0,0 +1,10 @@
+require "got/character/query/version"
+
+module Got
+  module Character
+    module Query
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/got/character/query/version.rb
@@ -0,0 +1,7 @@
+module Got
+  module Character
+    module Query
+      VERSION = "0.1.0"
+    end
+  end
+end
